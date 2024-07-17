9e253a628a8e6145078a4b681248d71261563ecc
Fernando Rodriguez <feransic@gmail.com>
2024-07-17 16:15:35 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,93 @@
+//Variables
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const display = document.getElementById("display");
+const cButton = document.getElementById("c");
+
+let firstData = false;
+let secondData = false;
+let result;
+let firstNumber = "0";
+let secondNumber = "0";
+let currentOperator = null;
+
+//Listen to numbers
+numbers.forEach((number) => {
+  number.addEventListener("click", () => {
+    if (firstData) {
+      if (secondNumber === "0") {
+        secondNumber = number.value;
+      } else {
+        secondNumber += number.value;
+      }
+      displayData(secondNumber);
+      secondData = true;
+    } else {
+      if (firstNumber === "0") {
+        firstNumber = number.value;
+      } else {
+        firstNumber += number.value;
+      }
+      displayData(firstNumber);
+    }
+  });
+});
+
+//Listen to operators
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    if (display.value === "") {
+      alert("Ingresa un numero");
+    } else {
+      if (operator.value !== "=") {
+        firstData = true;
+        currentOperator = operator.value;
+        displayData(null);
+      } else {
+        if (secondData) {
+          switch (currentOperator) {
+            case "+":
+              result = parseInt(firstNumber) + parseInt(secondNumber);
+              break;
+            case "-":
+              result = parseInt(firstNumber) - parseInt(secondNumber);
+              break;
+            case "*":
+              result = parseInt(firstNumber) * parseInt(secondNumber);
+              break;
+            case "/":
+              if (secondNumber === "0") {
+                result = "ERROR";
+              } else {
+                result = parseInt(firstNumber) / parseInt(secondNumber);
+              }
+              break;
+            default:
+              return;
+          }
+          displayData(result);
+          resetValues();
+        }
+      }
+    }
+  });
+});
+
+//C Button press
+cButton.addEventListener("click", () => {
+  resetValues();
+});
+
+//Functions
+function resetValues() {
+  firstData = false;
+  secondData = false;
+  result = 0;
+  firstNumber = "0";
+  secondNumber = "0";
+  currentOperator = null;
+}
+
+function displayData(data) {
+  display.value = data;
+}
